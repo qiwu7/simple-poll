6322fb9a93b309bd82558ca70c21e26ed941c3a5
0x57 <qi.wu@coinbase.com>
2024-02-20 17:04:49 +0800

    Add form (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,15 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Story time!',
-    },
-    {
-      action: 'link',
-      label: 'Link to Google',
-      target: 'https://www.google.com',
-    },
-    {
-      action: 'post_redirect',
-      label: 'Redirect to pictures',
+      label: 'Start Playing!',
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
+    src: `${NEXT_PUBLIC_URL}/cover.png`,
     aspectRatio: '1:1',
   },
   input: {
-    text: 'Tell me a boat story',
+    text: 'Letter',
   },
   postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
 });
@@ -33,7 +24,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Welcome',
     description: 'Hello Casters',
-    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+    images: [`${NEXT_PUBLIC_URL}/cover.png`],
   },
   other: {
     ...frameMetadata,
