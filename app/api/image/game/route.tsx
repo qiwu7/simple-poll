af4fcfb0f7a3347032c48a73cc5e103748deb179
Qi Wu <qi.wu@coinbase.com>
2024-02-26 12:38:57 +0800

    Image tweak

--- app/api/image/game/route.tsx
+++ app/api/image/game/route.tsx
@@ -28,13 +28,19 @@ export async function GET(request: Request) {
           <path d="M37.59.25l36.95 64H.64l36.95-64z"></path>
         </svg>
         <div style={{ display: 'flex', marginTop: 40 }}>Lifes: {lifesLeft}</div>
-        <div style={{ display: 'flex', marginTop: 40 }}>
-          {word?.split('').map((letter, index) => {
-            return <div key={index}>{(guesses?.includes(letter) ? letter : '_') + ' '}</div>;
-          })}
+        <div style={{ display: 'flex'}}>
+          <h1>
+            {word?.split('').map((letter, index) => {
+              return <div key={index}>{(guesses?.includes(letter) ? letter : '_') + ' '}</div>;
+            })}
+          </h1>
         </div>
-        <div style={{ display: 'flex', marginTop: 40 }}>{guesses?.join(', ')}</div>
-        <div style={{ display: 'flex', marginTop: 40 }}>{gameState(win, lifesLeft)}</div>
+        <div style={{ display: 'flex' }}>
+          {guesses?.join(', ')}
+        </div>
+        <div style={{ display: 'flex', marginTop: 40 }}>
+          <span tw="text-indigo-600">{gameState(win, lifesLeft)}</span>
+        </div>
       </div>
     ),
   );
@@ -46,6 +52,6 @@ function gameState(win: boolean, lifeLeft: number): string {
   } else if (lifeLeft <= 0) {
     return 'You lost';
   } else {
-    return 'Keep guessing';
+    return 'Keep guessing...';
   }
 }
